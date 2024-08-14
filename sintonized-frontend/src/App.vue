14fe74e3389297efdba87ad84be6b2b193b0ca61
João Sales <joaosales911@gmail.com>
2024-08-14 09:04:51 -0300

    feat: create header component

--- sintonized-frontend/src/App.vue
+++ sintonized-frontend/src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HeaderComponent/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderComponent from './components/HeaderComponent'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderComponent,
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: "Poppins", sans-serif;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
---
+++ sintonized-frontend/src/components/HeaderComponent.vue
@@ -0,0 +1,41 @@
+<template>
+    <header>
+        <div class="logo">
+            <img class="profile" src="../assets/icons8-music-record-96.png" alt="icon logo">
+            <h1>SINTONIZED</h1>
+        </div>
+        <img class="profile" src="../assets/profile.jpg" alt="profile">
+    </header>
+</template>
+<script>
+export default{
+    name:'HeaderComponent',
+}
+</script>
+<style>
+.profile{
+    border-radius: 50%;
+    height: 55px;
+}
+
+h1{
+    font-size: 27px;
+    color: #d1410c;
+    margin-left: 10px;
+}
+
+header, .logo{
+    display: flex;
+    flex-direction: row;
+}
+
+.logo{
+align-items:center
+}
+
+header{
+justify-content: space-between;
+margin: 10px;
+padding:  0px;
+}
+</style>
