066f528481f732831de30e553ea36800691562c7
João Lucas <joao.duarte31219@alunos.ufersa.edu.br>
2024-08-31 23:58:51 -0300

    add: função generate_audio_params

--- sintonizedbackend/Sintonized/views.py
+++ sintonizedbackend/Sintonized/views.py
@@ -1,3 +1,5 @@
+import os
+import requests
 from django.shortcuts import render
 from .models import AudioCommand
 from rest_framework.views import APIView
@@ -5,15 +7,26 @@ from rest_framework.response import Response
 from .serializers import AudioCommandSerializer
 
 # WIP
-def generate_audio_params(command):
-    if command == 'play':
-        return {'song': 'song.mp3'}
-    elif command == 'pause':
-        return {'action': 'pause'}
-    elif command == 'stop':
-        return {'action': 'stop'}
-    else:
-        return None
+def generate_audio_params(text):
+    url = "https://api.openai.com/v1/chat/completions"
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {os.environ.get("DJANGO_APP_GEMINI_API_KEY")}' 
+    }
+    data = {
+        "model": "gpt-3.5-turbo",
+        "messages": [{"role": "user", "content": text}]
+    }
+
+    try:
+        response = requests.post(url, headers=headers, json=data)
+        response.raise_for_status()  # Raise an exception for error HTTP status codes
+        response_data = response.json()
+        audio_params = response_data['choices'][0]['message']['content']
+        return audio_params
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+        return None  # Or handle the error in a more specific way
 
 # Create your views here.
 class AudioCommandView(APIView):
